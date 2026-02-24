89f60d834e562312357cce17c7e8d99d6d0ef113
developeragencia <agenciadeveloper01@gmail.com>
2026-02-24 14:49:23 -0300

    cleanup_build

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,7 +1,6 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-import { inspectAttr } from 'kimi-plugin-inspect-react'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,9 +9,8 @@ export default defineConfig({
     'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL || 'https://meufreelas.com.br/api'),
   },
   plugins: [
-    process.env.NODE_ENV === 'development' ? inspectAttr() : null,
     react(),
-  ].filter(Boolean),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
